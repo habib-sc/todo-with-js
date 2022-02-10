d39e749d5d36bb36944540db3c780c13d3c3d4a6
habib-sc <habib97.cs@gmail.com>
2022-02-10 19:57:14 +0600

    todo item adding funtionality added

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function todoAdd() {
+    // Getting todo input 
+    const todoInput = document.getElementById('todo-input');
+    // Getting todo container 
+    const todoContainer = document.getElementById('todo-container');
+    // Create Element 
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
+    <th scope="row">1</th>
+    <td>${todoInput.value}</td>
+    <td class="w-25"><button type="button" class="btn btn-success btn-sm">Done</button>
+        <button type="button" class="btn btn-primary btn-sm">Edit</button>
+        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+    </td>
+    `;
+    // appending element to container 
+    todoContainer.appendChild(tableRow);
+
+    // clearing the todo input field 
+    todoInput.value = '';
+
+}
