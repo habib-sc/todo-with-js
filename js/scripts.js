01594b76297f26c97a77ac0fbc343905586c09ea
habib-sc <habib97.cs@gmail.com>
2022-02-10 20:55:06 +0600

    delete functionality added and remove onclick function to add todo item

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // todo item index 
 let itemIndex = 1;
 
-function todoAdd() {
+document.getElementById('todo-add-btn').addEventListener('click', function() {
     // Getting todo input 
     const todoInput = document.getElementById('todo-input');
     // Getting todo container 
@@ -9,13 +9,13 @@ function todoAdd() {
     // Create Element 
     const tableRow = document.createElement('tr');
     tableRow.innerHTML = `
-    <th scope="row">${itemIndex}</th>
-    <td>${todoInput.value}</td>
-    <td class="w-25"><button type="button" class="btn btn-success btn-sm">Done</button>
-        <button type="button" class="btn btn-primary btn-sm">Edit</button>
-        <button type="button" class="btn btn-danger btn-sm">Delete</button>
-    </td>
-    `;
+     <th scope="row">${itemIndex}</th>
+     <td>${todoInput.value}</td>
+     <td class="w-25"><button type="button" class="btn btn-success btn-sm">Done</button>
+         <button type="button" class="btn btn-primary btn-sm">Edit</button>
+         <button type="button" class="todo-delete-btn btn btn-danger btn-sm">Delete</button>
+     </td>
+     `;
     // appending element to container 
     todoContainer.appendChild(tableRow);
 
@@ -25,4 +25,17 @@ function todoAdd() {
     // Item index increase by 1
     itemIndex++
 
-}
+    // Todo Delete feature 
+
+    // Getting delete buttons
+    const deleteButtons = document.getElementsByClassName('todo-delete-btn');
+    // button iteration 
+    for (const deleteButton of deleteButtons) {
+        deleteButton.addEventListener('click', function(event) {
+            // Getting todo item row 
+            const todoItem = event.target.parentNode.parentNode;
+            // displaying as none 
+            todoItem.style.display = 'none';
+        })
+    }
+})
