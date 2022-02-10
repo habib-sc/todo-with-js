a70bf3c0757ce20bf7b7604ce5c59fcea9a8dc83
habib-sc <habib97.cs@gmail.com>
2022-02-10 21:00:10 +0600

    Todo blank input validation

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,25 @@ document.getElementById('todo-add-btn').addEventListener('click', function() {
     const todoInput = document.getElementById('todo-input');
     // Getting todo container 
     const todoContainer = document.getElementById('todo-container');
-    // Create Element 
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-     <th scope="row">${itemIndex}</th>
-     <td>${todoInput.value}</td>
-     <td class="w-25"><button type="button" class="btn btn-success btn-sm">Done</button>
-         <button type="button" class="btn btn-primary btn-sm">Edit</button>
-         <button type="button" class="todo-delete-btn btn btn-danger btn-sm">Delete</button>
-     </td>
-     `;
-    // appending element to container 
-    todoContainer.appendChild(tableRow);
+    // validation 
+    if (todoInput.value == '') {
+        alert('Please Enter Your ToDo!')
+    } else {
+        // Create Element 
+        const tableRow = document.createElement('tr');
+        tableRow.innerHTML = `
+        <th scope="row">${itemIndex}</th>
+        <td>${todoInput.value}</td>
+        <td class="w-25"><button type="button" class="btn btn-success btn-sm">Done</button>
+            <button type="button" class="btn btn-primary btn-sm">Edit</button>
+            <button type="button" class="todo-delete-btn btn btn-danger btn-sm">Delete</button>
+        </td>
+        `;
+        // appending element to container 
+        todoContainer.appendChild(tableRow);
+    }
+
+
 
     // clearing the todo input field 
     todoInput.value = '';
